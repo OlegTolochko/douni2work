f1a2774b82deaed918fcdee2fadeeaafbdf9abc0
niels <33730531+yndolg@users.noreply.github.com>
2023-03-10 20:33:13 +0100

    Timespan-Chooser, Timings, Chart

--- backend.py
+++ backend.py
@@ -1,7 +1,10 @@
+import time
+
 from flask import Flask, g, current_app, jsonify
 import sqlite3
 
 app = Flask(__name__)
+
 
 def get_db():
     if 'db' not in g:
@@ -11,44 +14,91 @@ def get_db():
         )
         g.db.row_factory = sqlite3.Row
     return g.db
+
+
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
 
-@app.route("/statusHistory")
-def statusHistory():
 
+@app.route("/statusHistory/<timespan>")
+def statusHistory(timespan):
     cur = get_db().cursor()
 
-    selected = "week"
-    time = 0
-    if selected == "day":
-        time = 24*60*60
-    elif selected == "week":
-        time = 24*60*60*7
-    elif selected == "month":
-        time = 24*60*60*30
-    elif selected == "year":
-        time = 24*60*60*365
+    time = seconds_in_timespan(timespan)
 
-    cur.execute("""SELECT count(id),
-       avg(works),
-       date
+    cur.execute("""SELECT count(id) as datapoints,
+       avg(works) as uptime,
+       date,
+       category
 FROM (SELECT *,
-             CASE WHEN status_code == 200 THEN 1 ELSE 0 END AS works,
-             round(unixepoch(date) / ?/90)                   AS category
+             CASE WHEN status_code == 200 THEN 1 ELSE 0 END    AS works,
+             round((unixepoch(datetime('now')) - unixepoch(date)) / (?/90))                   AS category
       FROM measurements
-      WHERE unixepoch(date('now')) - unixepoch(date) < ?)
-GROUP BY category""", (time, time))
-    results = []
+      WHERE category < 90)
+GROUP BY category
+ORDER BY category""", (time,))
+
+    results = [None] * 90
     for row in cur.fetchall():
-        results.append(dict(row))
+        results[int(row["category"])] = dict(row)
+    response = jsonify(list(reversed(results)))
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+def seconds_in_timespan(timespan):
+    time = 0
+    if timespan == "day":
+        time = 24 * 60 * 60
+    elif timespan == "week":
+        time = 24 * 60 * 60 * 7
+    elif timespan == "month":
+        time = 24 * 60 * 60 * 30
+    elif timespan == "year":
+        time = 24 * 60 * 60 * 365
+    return time
+
+
+@app.route("/responseTimes/<timespan>")
+def responseTimes(timespan):
+    cur = get_db().cursor()
+
+    seconds = seconds_in_timespan(timespan)
+
+    cur.execute("""SELECT avg(response_time) as response_time,
+       round(avg(unixepoch(date))) as timestamp,
+       round((unixepoch(datetime('now')) - unixepoch(date)) / (? / 90)) as ind
+FROM measurements
+WHERE ind < 90
+GROUP BY ind;""", (seconds,))
+
+    results = [None]*90
+    for row in cur.fetchall():
+        results[int(row["ind"])] = dict(row)
+    for pos in range(len(results)):
+        if results[pos] is None:
+            results[pos] = {
+                'timestamp': int(time.time() - (seconds / 90) * pos),
+                'response_time': None
+            }
+
     response = jsonify(results)
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
+
+@app.route("/isOnline")
+def isOnline():
+    cur = get_db().cursor()
+
+    result = cur.execute("SELECT status_code, response_time, date FROM measurements ORDER BY date LIMIT 1")
+
+    response = jsonify(dict(cur.fetchall()[0]))
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 
 @app.route("/")
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
+//import "./styles.css";
+import React from "react";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+} from "recharts";
+import moment from "moment";
 
 function Option(props) {
     return <button type="button"
@@ -12,25 +22,68 @@ function Option(props) {
     </button>;
 }
 
+function Selector(props) {
+    return <div className="box-border hover:box-content py-2 px-2 bg-gray-100 rounded-lg">
+        <Option onClick={() => {
+            props.onChange("day")
+        }}
+                text="24h" active={props.selected === "day"}/>
+
+        <Option onClick={() => {
+            props.onChange("week")
+        }}
+                text="7d" active={props.selected === "week"}/>
+
+        <Option onClick={() => {
+            props.onChange("month")
+        }}
+                text="30d" active={props.selected === "month"}/>
+
+        <Option onClick={() => {
+            props.onChange("year")
+        }}
+                text="1y" active={props.selected === "year"}/>
+
+
+    </div>
+}
+
+function calcUptime(status) {
+    let total_events = 0;
+    let sum_uptime = 0;
+    for (const point of status) {
+        if (point) {
+            sum_uptime += point.uptime * point.datapoints;
+            total_events += point.datapoints;
+        }
+    }
+    return Math.round(sum_uptime * 100 * 100 / total_events) / 100;
+}
+
+
 function App() {
     const [status, setStatus] = useState();
 
-    useEffect(() => {
-        fetch("http://localhost:5000/statusHistory").then(r => r.json()).then(setStatus)
-    }, []);
-
     const [timespan, setTimespan] = useState("week");
+    const [timingTimespan, setTimingTimespan] = useState("week");
 
-    let filledStatus = Array();
-    if (status) {
-        for (let i = 0; i < 90-status.length; i++) {
-            filledStatus.push(null);
-        }
-        filledStatus = filledStatus.concat(status);
-    }
+    const [responseTimes, setResponseTimes] = useState();
 
+    const [lastRequest, setLastRequest] = useState();
+    useEffect(()=> {
+        fetch("http://localhost:5000/responseTimes/" + timingTimespan).then(r => r.json()).then(setResponseTimes);
+    }, [timingTimespan]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/statusHistory/" + timespan).then(r => r.json()).then(setStatus);
+    }, [timespan]);
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/isOnline").then(r => r.json()).then(setLastRequest);
+    }, [])
 
     return (<div>
+        {JSON.stringify(lastRequest)}
         <div className="container mx-auto max-w-3xl bg-white">
             <div className={"pt-10"}>
                 <h1 className={"text-8xl font-sans text-center pb-10 font-semibold"}>Uni2Work is <span
@@ -39,49 +92,97 @@ function App() {
             <div className={" border-solid border-2 rounded-lg p-5"}>
                 <div className="flex justify-end">
                     <h3 className={"grow"}> Uptime history</h3>
-                    <div className="box-border hover:box-content py-2 px-2 bg-gray-100 rounded-lg">
-                        <Option onClick={() => {
-                            setTimespan("day")
-                        }}
-                                text="24h" active={timespan === "day"}/>
 
-                        <Option onClick={() => {
-                            setTimespan("week")
-                        }}
-                                text="7d" active={timespan === "week"}/>
-
-                        <Option onClick={() => {
-                            setTimespan("month")
-                        }}
-                                text="30d" active={timespan === "month"}/>
-
-                        <Option onClick={() => {
-                            setTimespan("year")
-                        }}
-                                text="1y" active={timespan === "year"}/>
-
-
-                    </div>
+                    <Selector onChange={setTimespan} selected={timespan}/>
                     <div/>
                 </div>
                 <div className="flex mt-3 justify-end">
-                    {filledStatus.map((record, index) => {
-                        if(record == null){
-                            return <span key={index} className={"w-1 h-10 bg-gray-500 mr-1 rounded"}></span>;
-                        }
-                        else if (record.status_code == 200) {
+                    {status?.map((record, index) => {
+                        if (record == null) {
+                            return <span key={index} className={"w-1 h-10 bg-gray-300 mr-1 rounded"}></span>;
+                        } else if (record.uptime === 1) {
                             return <span key={index} className={"w-1 h-10 bg-green-500 mr-1 rounded"}></span>;
                         } else {
                             return <span key={index} className={"w-1 h-10 bg-red-500 mr-1 rounded"}></span>;
                         }
                     })}
                 </div>
-
+                <div className="mt-3 flex items-center">
+                    <div className="flex-1 h-0.5 bg-gray-300"></div>
+                    <div className="px-4"> {status ? calcUptime(status) : <></>}% uptime</div>
+                    <div className="flex-1 h-0.5 bg-gray-300"></div>
+                </div>
             </div>
             <div className={"mt-5 border-solid border-2 rounded-lg p-5"}>
-                <h3>Response times</h3>
+                <div className="flex justify-end">
+                    <h3 className={"grow"}> Response times</h3>
+
+                    <Selector onChange={setTimingTimespan} selected={timingTimespan}/>
+                    <div/>
+                </div>
+                <LineChart className="mt-3" width={700} height={300} data={responseTimes}>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <XAxis
+                        ticks={(() => {
+
+                            let date = moment();
+                            let result = [];
+                            if (timingTimespan === "day") {
+                                date.minute(0);
+                                for (let i = 0; i < 24; i += 3) {
+                                    result.push(moment(date).subtract(i, 'hours').unix());
+                                }
+                            } else if (timingTimespan === "week") {
+                                date.minute(0);
+                                date.hour(0);
+                                for (let i = 0; i < 7; i++) {
+                                    result.push(moment(date).subtract(i, "days").unix());
+                                }
+                            } else if (timingTimespan === "month") {
+                                date.minute(0);
+                                date.hour(0);
+                                for (let i = 0; i < 31; i += 2) {
+                                    result.push(moment(date).subtract(i, "days").unix());
+                                }
+                            } else if (timingTimespan === "year") {
+                                date.startOf("month");
+                                for (let i = 0; i < 13; i += 2) {
+                                    result.push(moment(date).subtract(i, "month").unix());
+                                }
+                            }
+
+                            return result;
+                        })()}
+                        tickFormatter={(time) => {
+                            const date = moment(time * 1000);
+                            if (timingTimespan === "day") {
+                                return date.format("hh:mm");
+                            } else if (timingTimespan === "week" || timingTimespan === "month") {
+                                return date.format("D. MMM");
+                            } else if (timingTimespan === "year") {
+                                return date.format("D. MMM");
+                            }
+                        }}
+                        type={'number'} domain={['dataMin', 'dataMax']} dataKey="timestamp"
+                        padding={{left: 30, right: 30}}/>
+                    <YAxis/>
+                    <Tooltip formatter={(x) => Math.round(x * 1000) / 1000}
+                             labelFormatter={(t) => moment(t * 1000).format("MMM Do YYYY, hh:mm:ss")}/>
+                    <Line
+                        strokeWidth={2}
+                        type="monotone"
+                        name="response time"
+                        unit={"s"}
+                        dot={false}
+                        dataKey="response_time"
+                        stroke="#8884d8"
+                        activeDot={{r: 4}}
+                        connectNulls={false}
+                    />
+                </LineChart>
             </div>
         </div>
+
     </div>)
 }
 
